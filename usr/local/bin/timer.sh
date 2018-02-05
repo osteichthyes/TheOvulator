4ab4d724790affdef2af3b22f0aa9bd9a0d386b3
Osteichthyes <osteichthyes@gmail.com>
2018-02-05 11:58:38 -0800

    Update timer.sh
    Eliminates daily variable, switches to just monthly.

--- usr/local/bin/timer.sh
+++ usr/local/bin/timer.sh
@@ -4,7 +4,7 @@
 ## | date | sunrise | sunset | 
 ## Then just grep to today's date, and get the sunrise, then at SUNRISE run the python
 
-DATE=$(date +%m-%d)
+DATE=$(date +%m)
 SUNRISE=$(cat /path/to/table/file | /bin/grep $DATE | awk -F\| '{print $2}')
 at -f /usr/local/bin/controlservo.sh $SUNRISE
 
