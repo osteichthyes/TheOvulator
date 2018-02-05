74ada08812f2917dae9cc6aaea8448e86df8bbf0
Osteichthyes <osteichthyes@gmail.com>
2018-02-05 12:07:15 -0800

    Update and rename tweetbot to tweetbot.sh

--- usr/local/bin/tweetbot.sh
+++ usr/local/bin/tweetbot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## This tweets directly to you, if you'd rather not, just set username to " "
-USERNAME="@YOUR_USERNAME"
-MESSAGE="You've Got Eggs $USERNAME"
+#USERNAME="@YOUR_USERNAME"
+MESSAGE="You've Got Eggs $twUser"
 /bin/twidge -c $HOME/.twidgerc update $MESSAGE
 
 exit 0
