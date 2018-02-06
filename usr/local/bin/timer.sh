00c098d8a4b2cd8d18491d019b962fc4def24ce8
Osteichthyes <osteichthyes@gmail.com>
2018-02-06 14:32:33 -0800

    Update timer.sh

--- usr/local/bin/timer.sh
+++ usr/local/bin/timer.sh
@@ -5,7 +5,7 @@
 ## Then just grep to today's date, and get the sunrise, then at SUNRISE run the python
 
 DATE=$(date +%m)
-SUNRISE=$(cat /path/to/table/file | /bin/grep $DATE | awk -F\| '{print $2}')
+SUNRISE=$(cat /usr/local/share/sunriseset.txt | /bin/grep $DATE | awk -F\| '{print $2}')
 at -f /usr/local/bin/controlservo.sh $SUNRISE
 
 exit 0
