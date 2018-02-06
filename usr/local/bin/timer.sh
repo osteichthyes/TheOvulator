6339cb0a9999271f7366d9694c464900bddfe28e
Osteichthyes <osteichthyes@gmail.com>
2018-02-06 15:50:06 -0800

    Update timer.sh

--- usr/local/bin/timer.sh
+++ usr/local/bin/timer.sh
@@ -4,6 +4,8 @@
 ## "date", "sunrise", "sunset"
 ## This is standard csv syntax
 ## Then just grep to today's date, and get the sunrise, then at SUNRISE run the python
+## DST is dumb, so since this is always called after 3AM, we always get invoked in the current time
+## By computing the time from the UTC time, we let the shell worry about timezone.
 
 curMonth=$(date +%m)
 Sunrise_UTC=$(cat /usr/local/share/sunriseset.txt | /bin/grep $curMonth | awk -F "\"*,\"*" '{print $2}')
